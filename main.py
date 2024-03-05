176fb1efde4f21d5c21f782e8e211c77c8558655
Alice00021 <alisa_barysheva03@mail.ru>
2024-03-05 12:04:22 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,90 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QTabWidget, QTableWidget, QTableWidgetItem, QWidget
+import psycopg2
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Библиотека")
+
+        # Создаем объект для соединения с базой данных
+        self.conn = psycopg2.connect(
+            dbname="Library",
+            user="postgres",
+            password="2118",
+            host="localhost"
+        )
+
+        self.central_widget = QTabWidget()
+        self.setCentralWidget(self.central_widget)
+
+        self.create_books_tab()
+        self.create_books_loans_tab()
+        self.create_readers_tab()
+
+    def create_books_tab(self):
+        tab = QWidget()
+        layout = QVBoxLayout()
+        table = QTableWidget()
+        table.setColumnCount(5)
+        table.setHorizontalHeaderLabels(["ID", "Название книги", "Автор", "Жанр", "Наличие"])
+        layout.addWidget(table)
+        tab.setLayout(layout)
+
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT * FROM Books")
+        books_data = cursor.fetchall()
+
+        for row_index, row_data in enumerate(books_data):
+            table.insertRow(row_index)
+            for column_index, column_data in enumerate(row_data):
+                table.setItem(row_index, column_index, QTableWidgetItem(str(column_data)))
+
+        self.central_widget.addTab(tab, "Книги")
+
+    def create_books_loans_tab(self):
+        tab = QWidget()
+        layout = QVBoxLayout()
+        table = QTableWidget()
+        table.setColumnCount(3)
+        table.setHorizontalHeaderLabels(["ID", "Дата начала", "Дата конца", "Дата возвращения" ,"ID книги", "ID читателя",])
+        layout.addWidget(table)
+        tab.setLayout(layout)
+
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT * FROM Books_loans")
+        loans_data = cursor.fetchall()
+
+        for row_index, row_data in enumerate(loans_data):
+            table.insertRow(row_index)
+            for column_index, column_data in enumerate(row_data):
+                table.setItem(row_index, column_index, QTableWidgetItem(str(column_data)))
+
+        self.central_widget.addTab(tab, "Выданные книги")
+
+    def create_readers_tab(self):
+        tab = QWidget()
+        layout = QVBoxLayout()
+        table = QTableWidget()
+        table.setColumnCount(2)
+        table.setHorizontalHeaderLabels(["ID", "Фамилия читателя", "Имя", "почта", "телефон"])
+        layout.addWidget(table)
+        tab.setLayout(layout)
+
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT * FROM Readers")
+        readers_data = cursor.fetchall()
+
+        for row_index, row_data in enumerate(readers_data):
+            table.insertRow(row_index)
+            for column_index, column_data in enumerate(row_data):
+                table.setItem(row_index, column_index, QTableWidgetItem(str(column_data)))
+
+        self.central_widget.addTab(tab, "Читатели")
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
