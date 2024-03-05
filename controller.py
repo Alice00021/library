6ea5b0af9b94e178f08c5c645643df0196629421
Alice00021 <alisa_barysheva03@mail.ru>
2024-03-05 12:19:18 +0300

    new files to refactor main.py

---
+++ controller.py
@@ -0,0 +1,14 @@
+import psycopg2
+
+class Data:
+    def connect_to_database():
+        pass
+    
+    def select_books():
+        pass
+    
+    def select_readers():
+        pass
+    
+    def select_books_loans():
+        pass
